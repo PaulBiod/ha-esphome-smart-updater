b8e242e159a4fa6587d2281a15cc33e8d4d0c264
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 01:59:51 +0200

    Update config_flow.py

--- custom_components/esphome_smart_updater/config_flow.py
+++ custom_components/esphome_smart_updater/config_flow.py
@@ -14,9 +14,6 @@ class ESPHomeSmartUpdaterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
-            return self.async_create_entry(
-                title="ESPHome Smart Updater",
-                data={},
-            )
+            return self.async_create_entry(title="ESPHome Smart Updater", data={})
 
         return self.async_show_form(step_id="user", data_schema=None)
