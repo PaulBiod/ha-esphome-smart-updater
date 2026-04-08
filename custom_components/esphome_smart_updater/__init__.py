16dd1f811c73b8a56af7658890fc757670c59eb4
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 01:45:37 +0200

    Update __init__.py

--- custom_components/esphome_smart_updater/__init__.py
+++ custom_components/esphome_smart_updater/__init__.py
@@ -15,7 +15,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = {}
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
 
     return True
 
