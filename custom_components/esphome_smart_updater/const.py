dc93fe4ffea253849e5ccb114f371978a13511da
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 09:55:58 +0200

    Update const.py

--- custom_components/esphome_smart_updater/const.py
+++ custom_components/esphome_smart_updater/const.py
@@ -1,5 +1,21 @@
 DOMAIN = "esphome_smart_updater"
 
-DEFAULT_TIMEOUT = 600  # 10 min
+DEFAULT_TIMEOUT = 600
 DEFAULT_DELAY_MIN = 5
 DEFAULT_DELAY_MAX = 60
+DEFAULT_MAX_ITEMS = 1
+
+CONF_TIMEOUT = "timeout"
+CONF_THROTTLE = "throttle"
+CONF_DELAY_MIN = "delay_min"
+CONF_DELAY_MAX = "delay_max"
+CONF_MAX_ITEMS = "max_items"
+CONF_CPU_SENSOR = "cpu_sensor"
+CONF_TEMP_SENSOR = "temp_sensor"
+CONF_LOAD_SENSOR = "load_sensor"
+
+CAMPAIGN_SENSOR_UNIQUE_ID = "esphome_smart_updater_campaign"
+BUTTON_START_UNIQUE_ID = "esu_start"
+BUTTON_PAUSE_UNIQUE_ID = "esu_pause"
+BUTTON_RESUME_UNIQUE_ID = "esu_resume"
+BUTTON_STOP_UNIQUE_ID = "esu_stop"
