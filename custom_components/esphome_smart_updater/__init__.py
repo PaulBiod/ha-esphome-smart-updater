783246876af21556fbd46c5ae51b195fecf4402a
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 09:56:20 +0200

    Update __init__.py

--- custom_components/esphome_smart_updater/__init__.py
+++ custom_components/esphome_smart_updater/__init__.py
@@ -7,7 +7,11 @@ from .coordinator import CampaignManager
 PLATFORMS = ["sensor", "button"]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
 
     manager = CampaignManager(hass, entry)
@@ -17,7 +21,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    hass.data[DOMAIN].pop(entry.entry_id)
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        manager = hass.data[DOMAIN].pop(entry.entry_id, None)
+        if manager is not None:
+            await manager.async_shutdown()
+    return unload_ok
