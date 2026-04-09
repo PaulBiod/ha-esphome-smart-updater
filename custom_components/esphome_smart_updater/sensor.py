89bf6159413b0b1076331096b80986a795842624
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 16:54:19 +0200

    Update sensor.py

--- custom_components/esphome_smart_updater/sensor.py
+++ custom_components/esphome_smart_updater/sensor.py
@@ -1,3 +1,4 @@
+# custom_components/esphome_smart_updater/sensor.py
 from __future__ import annotations
 
 from homeassistant.components.sensor import SensorEntity
@@ -5,7 +6,11 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import CAMPAIGN_SENSOR_UNIQUE_ID, DOMAIN
+from .const import (
+    CAMPAIGN_SENSOR_UNIQUE_ID,
+    DOMAIN,
+    PENDING_UPDATES_SENSOR_UNIQUE_ID,
+)
 
 
 async def async_setup_entry(
@@ -14,14 +19,15 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     manager = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([ESPHomeSmartUpdaterCampaignSensor(manager)])
+    async_add_entities(
+        [
+            ESPHomeSmartUpdaterCampaignSensor(manager),
+            ESPHomeSmartUpdaterPendingUpdatesSensor(manager),
+        ]
+    )
 
 
-class ESPHomeSmartUpdaterCampaignSensor(SensorEntity):
-    _attr_name = "ESPHome Smart Updater Campaign"
-    _attr_unique_id = CAMPAIGN_SENSOR_UNIQUE_ID
-    _attr_icon = "mdi:upload-network"
-
+class _BaseESUSensor(SensorEntity):
     def __init__(self, manager) -> None:
         self.manager = manager
         self._remove_listener = None
@@ -34,6 +40,12 @@ class ESPHomeSmartUpdaterCampaignSensor(SensorEntity):
             self._remove_listener()
             self._remove_listener = None
 
+
+class ESPHomeSmartUpdaterCampaignSensor(_BaseESUSensor):
+    _attr_name = "ESPHome Smart Updater Campaign"
+    _attr_unique_id = CAMPAIGN_SENSOR_UNIQUE_ID
+    _attr_icon = "mdi:upload-network"
+
     @property
     def native_value(self):
         return self.manager.state
@@ -41,3 +53,21 @@ class ESPHomeSmartUpdaterCampaignSensor(SensorEntity):
     @property
     def extra_state_attributes(self):
         return self.manager.campaign_attributes()
+
+
+class ESPHomeSmartUpdaterPendingUpdatesSensor(_BaseESUSensor):
+    _attr_name = "ESPHome Pending Updates Count"
+    _attr_unique_id = PENDING_UPDATES_SENSOR_UNIQUE_ID
+    _attr_icon = "mdi:update"
+
+    @property
+    def native_value(self):
+        return self.manager.pending_updates_count
+
+    @property
+    def extra_state_attributes(self):
+        entities = self.manager.pending_updates_entities()
+        return {
+            "pending_updates": entities,
+            "total": len(entities),
+        }
