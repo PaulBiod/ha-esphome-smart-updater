8f10204e06db1e6438bdd7e775411856d623faf9
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 09:38:53 +0200

    Update config_flow.py

--- custom_components/esphome_smart_updater/config_flow.py
+++ custom_components/esphome_smart_updater/config_flow.py
@@ -1,19 +1,33 @@
-from __future__ import annotations
-
 from homeassistant import config_entries
-from homeassistant.data_entry_flow import FlowResult
+import voluptuous as vol
 
-from .const import DOMAIN
+from .const import DOMAIN, DEFAULT_TIMEOUT
 
 
-class ESPHomeSmartUpdaterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class FlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
-    async def async_step_user(self, user_input=None) -> FlowResult:
-        if self._async_current_entries():
-            return self.async_abort(reason="single_instance_allowed")
+    async def async_step_user(self, user_input=None):
+        return self.async_create_entry(title="ESPHome Smart Updater", data={})
 
+    async def async_get_options_flow(self, entry):
+        return OptionsFlow(entry)
+
+
+class OptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, entry):
+        self.entry = entry
+
+    async def async_step_init(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="ESPHome Smart Updater", data={})
+            return self.async_create_entry(title="", data=user_input)
 
-        return self.async_show_form(step_id="user", data_schema=None)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional("timeout", default=600): int,
+                vol.Optional("throttle", default=True): bool,
+                vol.Optional("delay_min", default=5): int,
+                vol.Optional("delay_max", default=60): int,
+            })
+        )
