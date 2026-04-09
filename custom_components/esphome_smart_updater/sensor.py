696ecc88839e4ca4c62f18d82ad120bbe9e3f74b
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 02:39:55 +0200

    Update sensor.py

--- custom_components/esphome_smart_updater/sensor.py
+++ custom_components/esphome_smart_updater/sensor.py
@@ -1,51 +1,48 @@
-from __future__ import annotations
-
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(
-        [
-            ESPHomeSmartUpdaterStateSensor(coordinator),
-            ESPHomeSmartUpdaterCurrentSensor(coordinator),
-            ESPHomeSmartUpdaterProgressSensor(coordinator),
-        ]
-    )
+
+    async_add_entities([
+        ESUStateSensor(coordinator),
+        ESUCurrentSensor(coordinator),
+        ESUProgressSensor(coordinator),
+    ])
 
 
-class _BaseSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator) -> None:
+class BaseSensor(CoordinatorEntity, SensorEntity):
+    def __init__(self, coordinator):
         super().__init__(coordinator)
 
 
-class ESPHomeSmartUpdaterStateSensor(_BaseSensor):
+class ESUStateSensor(BaseSensor):
     _attr_name = "ESPHome Smart Updater State"
-    _attr_unique_id = "esphome_smart_updater_state"
+    _attr_unique_id = "esu_state"
 
     @property
     def native_value(self):
-        return self.coordinator.data.get("state", "idle")
+        return self.coordinator.data.get("state")
 
 
-class ESPHomeSmartUpdaterCurrentSensor(_BaseSensor):
+class ESUCurrentSensor(BaseSensor):
     _attr_name = "ESPHome Smart Updater Current"
-    _attr_unique_id = "esphome_smart_updater_current"
+    _attr_unique_id = "esu_current"
 
     @property
     def native_value(self):
         return self.coordinator.data.get("current")
 
 
-class ESPHomeSmartUpdaterProgressSensor(_BaseSensor):
+class ESUProgressSensor(BaseSensor):
     _attr_name = "ESPHome Smart Updater Progress"
-    _attr_unique_id = "esphome_smart_updater_progress"
+    _attr_unique_id = "esu_progress"
     _attr_native_unit_of_measurement = "%"
 
     @property
@@ -56,10 +53,4 @@ class ESPHomeSmartUpdaterProgressSensor(_BaseSensor):
 
     @property
     def extra_state_attributes(self):
-        return {
-            "finished": self.coordinator.data.get("finished", 0),
-            "total": self.coordinator.data.get("total", 0),
-            "done": self.coordinator.data.get("done", []),
-            "failed": self.coordinator.data.get("failed", []),
-            "queue": self.coordinator.data.get("queue", []),
-        }
+        return self.coordinator.data
