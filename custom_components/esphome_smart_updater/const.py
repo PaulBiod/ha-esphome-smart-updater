5353694fbfdf91f68651e5af8437bc8d529180dd
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 02:06:51 +0200

    Update const.py

--- custom_components/esphome_smart_updater/const.py
+++ custom_components/esphome_smart_updater/const.py
@@ -3,9 +3,3 @@ DOMAIN = "esphome_smart_updater"
 DEFAULT_TIMEOUT = 600  # 10 min
 DEFAULT_DELAY_MIN = 5
 DEFAULT_DELAY_MAX = 60
-
-CONF_ENABLE_THROTTLE = "enable_throttle"
-CONF_CPU_SENSOR = "cpu_sensor"
-CONF_TEMP_SENSOR = "temp_sensor"
-CONF_LOAD_SENSOR = "load_sensor"
-CONF_TIMEOUT = "timeout_s"
