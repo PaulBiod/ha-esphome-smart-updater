b8217d455b3a7cd81c4f7f607d29530e1a9be781
Paul leal Alves <scriptsgratuits@gmail.com>
2026-04-09 09:38:30 +0200

    Update __init__.py

--- custom_components/esphome_smart_updater/__init__.py
+++ custom_components/esphome_smart_updater/__init__.py
@@ -1,10 +1,23 @@
-from .coordinator import CampaignManager, DOMAIN
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-async def async_setup_entry(hass, entry):
-    manager = CampaignManager(hass)
-    hass.data[DOMAIN] = manager
+from .const import DOMAIN
+from .coordinator import CampaignManager
+
+PLATFORMS = ["sensor", "button"]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.data.setdefault(DOMAIN, {})
+
+    manager = CampaignManager(hass, entry)
+    hass.data[DOMAIN][entry.entry_id] = manager
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass, entry):
-    hass.data.pop(DOMAIN)
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
